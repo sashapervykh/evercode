d5266f36c39ce1677cb42bcd917acf3d6196f60a
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-17 20:06:12 +0300

    test: add test for scheduler

--- tests/scheduler.test.js
+++ tests/scheduler.test.js
@@ -7,4 +7,39 @@ describe("scheduleTask", () => {
       scheduleTask("", 1000, () => {});
     }).toThrow(ValidationError);
   });
+  test("should throw ValidationError if task name is not string", () => {
+    expect(() => {
+      scheduleTask(1, 1000, () => {});
+    }).toThrow(ValidationError);
+  });
+  test("should throw ValidationError if task interval is not a number", () => {
+    expect(() => {
+      scheduleTask("Test task", "1000", () => {});
+    }).toThrow(ValidationError);
+  });
+  test("should throw ValidationError if task interval is negative", () => {
+    expect(() => {
+      scheduleTask("Test task", -1000, () => {});
+    }).toThrow(ValidationError);
+  });
+  test("should throw ValidationError if task interval is NaN", () => {
+    expect(() => {
+      scheduleTask("Test task", NaN, () => {});
+    }).toThrow(ValidationError);
+  });
+  test("should throw ValidationError if task is not a function", () => {
+    expect(() => {
+      scheduleTask("Test task", NaN, "task");
+    }).toThrow(ValidationError);
+  });
+  test("should log schedule task", () => {
+    jest.useFakeTimers();
+    const task = jest.fn();
+    scheduleTask("Test task", 1000, task);
+    jest.advanceTimersByTime(1000);
+    expect(task).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+    expect(task).toHaveBeenCalledTimes(2);
+    jest.useRealTimers();
+  });
 });
