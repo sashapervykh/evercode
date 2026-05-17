34caa337388811a6807008291a4a531140c955e8
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-17 20:31:01 +0300

    test: add coverage requirements to ject config

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.js"],
   coverageReporters: ["text", "lcov", "html"],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 60,
+      functions: 60,
+      lines: 60,
+    },
+  },
 };
