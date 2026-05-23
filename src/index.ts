e5691ed8c95393b9a72d114585fc3db23cccac47
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-05-23 20:45:17 +0300

    chore: add express to project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import express from "express";
 import { startScheduler } from "./controllers/scheduler.controller.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express + TypeScript");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
 
 startScheduler();
